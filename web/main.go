3a9a0ab6ea20a69ed8ea94dc6e401e97d5d964db
doug-benn <fiery.coder@gmail.com>
2026-03-06 18:31:06 +0000

    Use Jacques Webpage
    WASM build
    Simple Webpage
    Deploy workflow

---
+++ web/main.go
@@ -0,0 +1,32 @@
+//go:build js && wasm
+// +build js,wasm
+
+package main
+
+import (
+	"syscall/js"
+
+	"github.com/doug-benn/Jacques/internal/processor"
+)
+
+func main() {
+	js.Global().Set("cleanSchema", js.FuncOf(cleanSchema))
+	js.Global().Set("ready", js.FuncOf(ready))
+	select {}
+}
+
+func cleanSchema(this js.Value, args []js.Value) interface{} {
+	if len(args) < 2 {
+		return ""
+	}
+
+	sql := args[0].String()
+	experimental := args[1].Bool()
+
+	opts := &processor.Options{ExperimentalFolding: experimental}
+	return processor.Process(sql, opts)
+}
+
+func ready(this js.Value, args []js.Value) interface{} {
+	return true
+}
